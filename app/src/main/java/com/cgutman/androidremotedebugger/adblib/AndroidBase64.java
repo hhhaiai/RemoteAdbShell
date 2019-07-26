da7c5aa29119ab9056c7f1cdec461f4a5729276f
sanbo <sanbo.xyz@gmail.com>
2019-07-26 13:18:51 +0800

    格式化代码。引入源码

--- app/src/main/java/com/cgutman/androidremotedebugger/adblib/AndroidBase64.java
+++ app/src/main/java/com/cgutman/androidremotedebugger/adblib/AndroidBase64.java
@@ -5,8 +5,8 @@ import android.util.Base64;
 import com.cgutman.adblib.AdbBase64;
 
 public class AndroidBase64 implements AdbBase64 {
-	@Override
-	public String encodeToString(byte[] data) {
-		return Base64.encodeToString(data, Base64.NO_WRAP);
-	}
+    @Override
+    public String encodeToString(byte[] data) {
+        return Base64.encodeToString(data, Base64.NO_WRAP);
+    }
 }
